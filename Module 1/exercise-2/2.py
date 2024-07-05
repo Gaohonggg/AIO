4fa5520135eac67cd35632b7d21e1eb01adae871
Gaohonggg <162142451+Gaohonggg@users.noreply.github.com>
2024-07-05 19:38:49 +0700

    Delete Module 1 directory

--- Module 1/exercise-2/2.py
+++
@@ -1,4 +0,0 @@
-def makedictionary(s):
-    return {s[i]:s.count(s[i]) for i in range(len(s))}
-s = input("Enter any string you want: ")
-print("The dictionary of your string is: {}".format(makedictionary(s)))
