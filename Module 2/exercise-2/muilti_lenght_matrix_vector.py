24949d3403ed2956f2f7372e9d89549ca9d09524
Gaohonggg <162142451+Gaohonggg@users.noreply.github.com>
2024-07-15 21:41:17 +0700

    Add files via upload

--- Module 2/exercise-2/muilti_lenght_matrix_vector.py
+++ Module 2/exercise-2/muilti_lenght_matrix_vector.py
@@ -44,3 +44,10 @@ matrix1 = np.array([[1,1],[2,2],[3,3]])
 print(matrix_multi_matrix(matrix,matrix1))
 maxtrix3 = [[-2,6],[8,-4]]
 print(inverse_matrix(maxtrix3))
+
+m1 = np.array([[1,2],[3,4]])
+m1 = np.reshape(m1,(-1,4),"F")[0]
+print( m1 )
+m2 = np.array([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]])
+result = m1 @ m2
+print( result )
