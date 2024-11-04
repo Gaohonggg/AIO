265396087f67a041e00216e697e57b56ca1e056c
Gaohonggg <162142451+Gaohonggg@users.noreply.github.com>
2024-11-04 17:26:15 +0700

    Create Twitter_Sentiment_Analysis.py

---
+++ Module 5/exercise-2/Twitter_Sentiment_Analysis.py
@@ -0,0 +1,65 @@
+train_accs = []
+train_losses = []
+val_accs = []
+val_losses = []
+
+for epoch in range(epochs):
+    train_batch_accs = []
+    train_batch_losses = []
+    val_batch_accs = []
+    val_batch_losses = []
+    for i in range(0,X_train.shape[0],batch_size):
+        X_i = X_train[i:i+batch_size]
+        y_i = y_train[i:i+batch_size]
+
+        y_hat = predict(X_i,theta)
+        train_loss = compute_loss(y_hat,y_i)
+
+        gradient = compute_gradient(X_i,y_i,y_hat)
+        theta = update_theta(theta,gradient,lr)
+
+        train_batch_losses.append(train_loss)
+
+        train_acc = compute_accuracy(X_train,y_train,theta)
+        train_batch_accs.append(train_acc)
+
+        y_val_hat = predict(X_val,theta)
+        val_loss = compute_loss(y_val_hat,y_val)
+        val_batch_losses.append(val_loss)
+        val_acc = compute_accuracy(X_val,y_val,theta)
+        val_batch_accs.append(val_acc)
+
+    train_batch_loss = sum(train_batch_losses)/len(train_batch_losses)
+    val_batch_loss = sum(val_batch_losses) / len(val_batch_losses)
+    train_batch_acc = sum(train_batch_accs) / len(train_batch_accs)
+    val_batch_acc = sum(val_batch_accs) / len(val_batch_accs)
+
+    train_losses.append(train_batch_loss)
+    val_losses.append(val_batch_loss)
+    train_accs.append(train_batch_acc)
+    val_accs.append(val_batch_acc)
+    print(f"\n EPOCH{epoch + 1}:\t Trainingloss: {train_batch_loss: .3f}\t Validationloss: {val_batch_loss: .3f}")
+
+fig, ax = plt.subplots(2,2,figsize=(12,10))
+ax[0 , 0].plot( train_losses )
+ax[0 , 0].set( xlabel="Epoch", ylabel="Loss")
+ax[0 , 0].set_title("Training Loss")
+
+ax[0 , 1].plot( val_losses, "orange")
+ax[0 , 1].set( xlabel="Epoch", ylabel="Loss")
+ax[0 , 1].set_title("Validation Loss")
+
+ax[1 , 0].plot( train_accs )
+ax[1 , 0].set( xlabel ="Epoch", ylabel="Accuracy")
+ax[1 , 0].set_title("Training Accuracy")
+
+ax[1 , 1].plot( val_accs, "orange")
+ax[1 , 1].set( xlabel ="Epoch", ylabel="Accuracy")
+ax[1 , 1].set_title("Validation Accuracy")
+
+plt.show()
+
+val_set_acc = compute_accuracy(X_val,y_val, theta)
+test_set_acc = compute_accuracy(X_test,y_test,theta)
+print(val_set_acc)
+print(test_set_acc)
